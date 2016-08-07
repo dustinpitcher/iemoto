308eb0cc1e7c41cb29561a0119b8c01ced766d23
dustinpitcher1 <dustinpitcher@gmail.com>
2016-08-07 18:52:25 +0000

    Changing Gruntfile to use grunt-sass instead of compass because it's way way faster, added livereload and made it so it'll reload on PHP file changes.

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,80 +1,46 @@
-/* vim: set ft=javascript expandtab shiftwidth=2 tabstop=2: */
-
-module.exports = function( grunt ) {
-
-  // Project configuration
-  grunt.initConfig( {
-    pkg: grunt.file.readJSON( 'package.json' ),
-
-    // javascript
-    jshint: {
-      dist: [
-        'js/{%= file_name %}.js'
-      ],
-      options: {
-        jshintrc: true
-      }
-    },
-
-    // compass(sass)
-    compass: {
-      dev: {
-        options: {
-          sassDir:        'sass',
-          cssDir:         'css',
-          imagesDir:      'images',
-          outputStyle:    'expanded',
-          relativeAssets: true,
-          noLineComments: true,
-          sourcemap:      true,
-          force:          true
-        }
-      },
-      dist: {
-        options: {
-          sassDir:        'sass',
-          cssDir:         './',
-          imagesDir:      'images',
-          outputStyle:    'expanded',
-          relativeAssets: true,
-          noLineComments: true,
-          sourcemap:      false,
-          force:          true
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      all: {
+        options:{
+          port: 8080,
+          hostname: "0.0.0.0",
+          livereload: 8081
         }
       }
     },
-    replace: {
-      dist: {
-        src: ['css/style.css', 'style.css'],
-        overwrite: true,
-        replacements: [{
-          from: /<%= pkg.version %>/g,
-          to: '<%= pkg.version %>'
-         }]
-      }
-    },
-
-    // watch
-    watch: {
-      dist: {
-        files: [
-          'sass/{,*/}{,*/}*.scss',
-          'js/{%= file_name %}.js'
-        ],
-        tasks: ['jshint', 'compass', 'replace']
-      }
-    }
-
-  } );
-
-  // Load other tasks
-  require('load-grunt-tasks')(grunt);
-
-  // Default task.
-  grunt.registerTask(
-    'default',
-    ['jshint', 'compass', 'replace']
-  );
-
-  grunt.util.linefeed = '\n';
+		watch: {
+			sass: {
+				files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
+				tasks: ['sass:dist'],
+				options: {
+					livereload: true,
+				}
+			},
+			livereload: {
+				files: ['*.css','*.html', '*.php', '*/*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+				options: {
+					livereload: true,
+          port: 8080,
+          hostname: "0.0.0.0",
+          livereload: 8081
+				}
+			}
+		},
+		sass: {
+			options: {
+				sourceMap: true,
+				outputStyle: 'expanded'
+			},
+			dist: {
+				files: {
+					'style.css': 'sass/style.scss'
+				}
+			}
+		}
+	});
+	grunt.registerTask('default', ['sass:dist', 'watch']);
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 };
